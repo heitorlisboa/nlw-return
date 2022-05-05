fbfcc3dce6f0ef2936a1c006ca29e3faa8144eda
heitorlisboa <heitor01101000@gmail.com>
2022-05-05 15:24:10 -0300

    feat(server): add cors middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './router';
 
 const app = express();
 const PORT = 4000;
 
+// TODO: Change cors origin to the website address when it is hosted
+app.use(cors());
 app.use(express.json());
-
 app.use(router);
 
 app.listen(PORT, () => {
